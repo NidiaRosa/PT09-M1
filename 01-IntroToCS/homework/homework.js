245844a2ffcb719d63f4355d7b44456a7d3d59b6
NidiaRosa <rosabittar0118@hotmail.com>
2022-08-03 15:53:45 -0500

    Complete

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,18 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  return parseInt(num,2);
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+    let binary = (num % 2).toString();
+    for (; num > 1; ) {
+        num = parseInt(num / 2);
+        binary =  (num % 2) + (binary);
+    }
+    return binary;
 }
 
 
