059ef2ec7c1180913352990ba511f6d1f0277471
NidiaRosa <rosabittar0118@hotmail.com>
2022-08-30 20:54:51 -0500

    homework AlgortimosII
    homework AlgortimosII

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,20 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) {
+    return array;
+  }
 
+  let pivot = array[0];
+  
+  let left = []; 
+  let right = [];
+
+  for (let i = 1; i < array.length; i++) {
+    array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
+  }
+
+  return quickSort(left).concat(pivot, quickSort(right));
 }
 
 function mergeSort(array) {
@@ -14,7 +27,24 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  const half = array.length / 2;
+  
+  if(array.length < 2) return array;
+  
+  const left = array.splice(0, half);
+  return merge(mergeSort(left),mergeSort(array));
+}
 
+function merge(left, right) {
+  let arr = []
+  while (left.length && right.length) {
+      if (left[0] < right[0]) {
+          arr.push(left.shift())  
+      } else {
+          arr.push(right.shift()) 
+      }
+  }
+  return [ ...arr, ...left, ...right ]
 }
 
 // No modificar nada debajo de esta línea
